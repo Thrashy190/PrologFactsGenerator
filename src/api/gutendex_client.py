2b693118f433924f41717202f86736c46969e821
Thrashy190 <diego456.dlm77@gmail.com>
2025-05-07 18:58:48 -0600

    update api

--- src/api/gutendex_client.py
+++ src/api/gutendex_client.py
@@ -5,9 +5,9 @@ from src.api.interfaces import APIClient
 
 class GutendexAPIClient(APIClient):
     def __init__(self):
-        self.base_url = "https://gutendex.com/books"
+        self.base_url = ""
 
-    def fetch_data(self, limit=2500):
+    def fetch_data(self, limit=10):
         results = []
         url = self.base_url
         while url and len(results) < limit:
@@ -16,7 +16,7 @@ class GutendexAPIClient(APIClient):
                 response.raise_for_status()
                 data = response.json()
                 results.extend(data.get("results", []))
-                url = data.get("next")
+                
                 time.sleep(0.5)
             except requests.RequestException as e:
                 print(f"Error al obtener datos: {e}")
